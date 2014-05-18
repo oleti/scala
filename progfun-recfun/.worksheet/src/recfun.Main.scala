d300baa286dcf631be87fc0892d1332f9bde65f9
Manoj Oleti <manoj.oleti@gmail.com>
2014-05-18 16:46:48 +0530

    Deleting incorrectly checked in cache and worksheet files

--- progfun-recfun/.worksheet/src/recfun.Main.scala
+++
@@ -1,5 +0,0 @@
-package recfun
-
-object Main {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(73); 
-  println("Welcome to the Scala worksheet")}
-}
